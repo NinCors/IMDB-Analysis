4a938d94e22fe129da1ab55fc3ec24bee47519bf
NinCors <chiyuc2@gmail.com>
2019-03-11 15:14:42 -0700

    finish word embedding

--- src/datahandler.py
+++ src/datahandler.py
@@ -6,8 +6,8 @@ import numpy as np
 print(tf.__version__)
 
 words_dict = "../imdb_data/imdb.vocab"
-train_data_dir_neg = "../imdb_data/train/neg"
-train_data_dir_pos = "../imdb_data/train/pos"
+train_data_dir_neg = "../imdb_data/train/neg/"
+train_data_dir_pos = "../imdb_data/train/pos/"
 
 # Handling data
 #   Load the dictionary from words_dict
@@ -20,7 +20,7 @@ train_data_dir_pos = "../imdb_data/train/pos"
 
 #Using the index from tensorflow
 #Credit to https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/keras/basic_text_classification.ipynb?pli=1#scrollTo=tr5s_1alpzop
-dictionary = keras.datasets.imdb.get_word_index()
+word_index = keras.datasets.imdb.get_word_index()
 word_index = {k:(v+3) for k,v in word_index.items()} 
 word_index["<PAD>"] = 0
 word_index["<START>"] = 1
@@ -28,16 +28,53 @@ word_index["<UNK>"] = 2  # unknown
 word_index["<UNUSED>"] = 3
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
+def encode_review(text):
+    for i in range(len(text)):
+        text[i] = word_index[text[i].lower()]
+    return text
+
+def decode_review(text):
+    for i in range(len(text)):
+        text[i] = reverse_word_index[text[i]]
+    return text
 
 def get_list(data_dirctory):
     dirct = os.listdir(data_dirctory)
     return dirct
 
 def embedding_words(words):
+    #Credit to https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/keras/basic_text_classification.ipynb?pli=1#scrollTo=tr5s_1alpzop
+    vocab_size = 100000
+    model = keras.Sequential()
+    model.add(keras.layers.Embedding(vocab_size, 16))
+    model.compile('rmsprop', 'mse')
+    res = model.predict(words)
+    return res
 
 def padding(words, size=256):
-    
+    size = len(words)
+    while(size < 256):
+        words.append(0)
+        size += 1
+    return words
+
 def get_review(file_name):
+    with open(file_name) as f:
+        content = f.readlines()
+    print(content[0])
+    review = content[0].split()
+
+    for i in range(len(review)):
+        if(not review[i].isalpha()):
+            review[i] = review[i][0:len(review[i])-1]
+    #print(review)
+    review = encode_review(review)
+    print(np.shape(review))
+    review = padding(review)
+    print(np.shape(review))
+    print (review)
+    review = embedding_words(review)
+    print(np.shape(review))
 
 def get_data(data_dirctory):
     train_data = []
@@ -53,8 +90,5 @@ def get_data(data_dirctory):
     print(train_data.shape)
     return train_data, train_label
 
-
-
-
-
+get_review(train_data_dir_neg+"0_3.txt")
 
